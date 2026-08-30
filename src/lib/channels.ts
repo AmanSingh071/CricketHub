1240eb808a0f3d7b49ef20acec1b3ef76c64d6ef
AmanSingh071 <89797086+AmanSingh071@users.noreply.github.com>
2026-08-30 21:52:38 +0530

    Add central channel URL configuration

---
+++ src/lib/channels.ts
@@ -0,0 +1,12 @@
+export type Channel={id:string;channel_name:string;url:string};
+export const channels:Channel[]=[
+{id:"willow-cricket",channel_name:"Willow Cricket",url:""},
+{id:"willow-2-cricket",channel_name:"Willow 2 Cricket",url:""},
+{id:"sony-ten-1",channel_name:"SONY TEN 1",url:""},
+{id:"sony-ten-2",channel_name:"SONY TEN 2",url:""},
+{id:"sony-ten-3",channel_name:"SONY TEN 3",url:""},
+{id:"star-sports-1-in",channel_name:"Star Sports 1 IN",url:""},
+{id:"star-sports-hindi-in",channel_name:"Star Sports Hindi IN",url:""},
+{id:"sky-sports-cricket",channel_name:"Sky Sports Cricket",url:""},
+{id:"sky-sports-cricket-uk",channel_name:"Sky Sports Cricket UK",url:""}
+];
